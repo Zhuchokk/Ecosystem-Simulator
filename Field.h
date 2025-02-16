2ee17603e69171b82782297a89af0fa784f01c93
Votter <zigl.zigl535@gmail.com>
2025-02-16 11:08:07 +0300

    Adding water maker

--- Field.h
+++ Field.h
@@ -2,18 +2,19 @@
 #include "Creatures.h"
 
 class Field {
-public:
-    int height;
-    int width;
+private:
     int water_density;
     double food_density;
-
     Object*** matrix;
-
+    int height;
+    int width;
+    
+    void generation();
+    int water_maker(int x, int y, int r);
+public:
     Field(int _height, int _width, double _food_density, double water_percentage);
     ~Field();
 
-    void generation();
     Object* get(int i, int j);
 
     bool move_object_right(int i, int j);
